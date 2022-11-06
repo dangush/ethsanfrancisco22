44796646c0c53f4a9783776bd6e0052152ffdba6
Daniel Gushchyan <d.gushchyan@gmail.com>
2022-11-05 19:17:48 -0700

    finally fixed my git issues

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,44 +1,71 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import 'lib/liquidity-direction-protocol/contracts/interfaces/IIbAllu';
 
 
-contract Alluo {
-    // First we will deposit 1000 USDC into the IbAlluo contract and
-    // receive IbAlluos.
-    let yourAddress= "";
-    let recipientAddress="";
-    let ibAlluoUSD = await ethers.getContractAt("IbAlluo", "0xC2DbaAEA2EfA47EBda3E572aa0e55B742E408BF6");
-    const usdc = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174";
-    let amount = parseEther(1000);
-    await ibAlluoUSD.deposit(usdc, amount);
+import { 
+    ISuperfluid 
+} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
 
-    // SMART CONTRACT VERSION ***************
+import { 
+    IConstantFlowAgreementV1 
+} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IConstantFlowAgreementV1.sol";
 
+import {
+    CFAv1Library
+} from "@superfluid-finance/ethereum-contracts/contracts/apps/CFAv1Library.sol";
+
+contract SomeContractWithCFAv1Library {
+
+    using CFAv1Library for CFAv1Library.InitData;
     
+    //initialize cfaV1 variable
+    CFAv1Library.InitData public cfaLib;
+    
+    constructor(
+        ISuperfluid host
+    ) {
+    
+        //initialize InitData struct, and set equal to cfaV1
+        cfaLib= CFAv1Library.InitData(
+        host,
+            //here, we are deriving the address of the CFA using the host contract
+            IConstantFlowAgreementV1(
+                address(host.getAgreementClass(
+                    keccak256("org.superfluid-finance.agreements.ConstantFlowAgreement.v1")
+                ))
+            )
+        );
+        
+    }
+    
+    //your contract code here...
+}
 
 
-    // SMART CONTRACT VERSION ***************
+contract HotPotato {
+
+    // 0: Game has not been initialized
+    // 1: Game is in lobby, users are joining
+    // 2: Game is active
+    uint8 gameState;
+
+    address payable hardcode;
+
+    function setGameState(uint8 _gameState) internal {
+        gameState = _gameState;
+    }
 
 
+    function permissionSetup():
 
-    // Now grant permissions to the IbAlluo contract to create streams on your behalf
-    // You only need to do this once for each IbAlluo contract when you first make a stream.
-    let encodeData = await ibAlluoCurrent.formatPermissions();
-    let superhost = await ethers.getContractAt("Superfluid", "0x3E14dC1b13c488a8d5D310918780c983bD5982E7");
-    await superhost.connect(signers[1]).callAgreement(
-        "0x6EeE6060f715257b970700bc2656De21dEdF074C",
-        encodeData,
-        "0x"
-    )
+    // *** SUPERFLUID LOGIC ***
 
-    // Now create the actual flow for 1 IbAlluo per second. The last parameter is the
-    // amount of IbAlluos you want to wrap into StIbAlluos. We recommend wrapping
-    // your entire balance.
-    let currentBalance = await ibAlluoUSD.combinedBalanceOf(yourAddress);
-    // If you only want to wrap 800 IbAlluos.
-    await ibAlluoUSD.createFlow(recipientAddress, parseEther("1"), parseEther("800"))
-    // Or if you want to wrap your entire balance
-    await ibAlluoUSD.createFlow(recipientAddress, parseEther("1"), currentBalance)
+    function initStream() public {
+        // initialize superfluid stream, send to hardcode
+    }
+
+
 
 }
